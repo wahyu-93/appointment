e781f9b2ff521bc7173cbb555b654272a0e5d05a
wahyu-93 <wahyu.wahyudi1005@gmail.com>
2023-08-23 11:56:25 +0800

    make model

---
+++ app/Models/ConfigPayment.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ConfigPayment extends Model
+{
+    use HasFactory;
+
+    protected $table = 'config_payment';
+}
